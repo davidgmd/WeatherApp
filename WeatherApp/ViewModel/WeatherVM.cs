d4a421fc8b700f1e148bc4228bcd91e22e1bda13
davidgmd <K_H_Survivor@hotmail.com>
2020-05-13 12:07:56 +0200

    Finalizando la lección 72

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using WeatherApp.Model;
 
 namespace WeatherApp.ViewModel
 {
-    class WeatherVM
+    public class WeatherVM : INotifyPropertyChanged
     {
 
+        private string query;
+
+        public string Query
+        {
+            get { return Query; }
+            set { 
+                Query = value;
+                OnPropertyChanged(Query);
+            }
+        }
+
+        private CurrentConditions currentconditions;
+
+        public CurrentConditions currentConditions
+        {
+            get { return currentConditions; }
+            set { 
+                currentConditions = value;
+                OnPropertyChanged("currentConditions");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged (string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
     }
 }
